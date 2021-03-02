3312feb9b16d7d69d9aac190286ba10396b81aaf
William Addesso <wsaddesso@gmail.com>
2021-03-02 18:29:05 -0500

    Moved all project functions into the project.js file.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import { format } from 'date-fns'
-import { projectFactory, createProjectDom } from './projects.js'
+import { projects } from './projects.js'
 
 // Set the current date at the top of the page.
 const formattedDate = format(new Date(), 'EEEE MM/dd/yyyy')
 const dateHeader = document.getElementById('date')
 dateHeader.innerHTML = `${formattedDate}`
 
-// Projects menu element.
-const projectsMenu = document.getElementById('projects-menu');
+projects();
 
-// Array containing all the projects created and saved to storage.
-const projectsArr = [];
-
-// Iterate through allProjects and create DOM elements.
-projectsArr.forEach(function(ele) {
-    createProjectDom(ele.newEle, projectsMenu);
-})
 
--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,40 @@
 // This module will contain factory functions to create projects and to do items.
 
-const projectFactory = (name) => {
-    const newEle = document.createElement("li");
-    newEle.innerHTML = name;
-    newEle.style.cursor = 'pointer'
-    return { name, newEle };
+
+function projects() {
+    const projectFactory = (name) => {
+        const newEle = document.createElement("li");
+        newEle.innerHTML = name;
+        newEle.style.cursor = 'pointer'
+        return { name, newEle };
+    }
+
+    const createProjectDom = (ele, parent) => {
+        parent.appendChild(ele);
+    }
+
+    // Projects menu element.
+    const projectsMenu = document.getElementById('projects-menu');
+
+    // Array containing all the projects created and saved to storage.
+    const projectsArr = [];
+
+    // Iterate through allProjects and create DOM elements.
+    projectsArr.forEach(function(ele) {
+        createProjectDom(ele.newEle, projectsMenu);
+    })
+
+    // Event listener for add project button.
+    const addProject = document.getElementById('addProject')
+    addProject.addEventListener("click", function() {
+        console.log('Clicked "Add Project"');
+    })
+
+    // Event listener for "Add Project" button
+    const addProjectBtn = document.getElementById("addProject");
+    addProjectBtn.addEventListener("click", function() {
+        alert("adding new project")
+    })
 }
 
-const createProjectDom = (ele, parent) => {
-    parent.appendChild(ele);
-}
-
-export { projectFactory, createProjectDom }
+export { projects };
